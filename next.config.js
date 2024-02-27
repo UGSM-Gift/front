0142c9ba3d787db651f139e52d371f0f937b005b
sonakeee <96439764+sonakeee@users.noreply.github.com>
2024-02-27 21:10:23 +0900

    add output

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   experimental: {
     appDir: true,
   },
   images: { unoptimized: true }
 
 }
-
 module.exports = nextConfig
